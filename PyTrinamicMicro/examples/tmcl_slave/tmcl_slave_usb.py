2b48a55f6795603f62ab84291d185d92e952ec9a
Leonard Kugis <leonard@kug.is>
2020-10-14 13:25:22 +0200

    logging
    Added logging in all tmcl_slave examples.

--- PyTrinamicMicro/examples/tmcl_slave/tmcl_slave_usb.py
+++ PyTrinamicMicro/examples/tmcl_slave/tmcl_slave_usb.py
@@ -15,6 +15,7 @@ Created on 06.10.2020
 from PyTrinamicMicro.connections.usb_vcp_tmcl_interface import usb_vcp_tmcl_interface
 from PyTrinamicMicro.TMCL_Slave import TMCL_Slave_Main
 import struct
+import logging
 
 # Constants
 MODULE_ADDRESS = 1
@@ -23,17 +24,29 @@ MODULE_ID_STRING = "0021"
 VERSION_STRING = MODULE_ID_STRING + "V100"
 BUILD_VERSION = 0
 
+# Prepare Logger
+logger = logging.getLogger(__name__)
+logger.info("TMCL Slave on USB_VCP interface")
+
 # Main program
 
+logger.info("Initializing interface ...")
 con = usb_vcp_tmcl_interface()
 slave = TMCL_Slave_Main(MODULE_ADDRESS, HOST_ADDRESS, VERSION_STRING, BUILD_VERSION)
+logger.info("Interface initialized.")
 
 while(not(slave.get_status().stop)):
     if(con.request_available()):
+        logger.debug("Request available.")
         request = con.receive_request()
         if(not(slave.filter(request))):
             continue
+        logger.debug("Request for this slave detected.")
         reply = slave.handle_request(request)
         con.send_reply(reply)
 
+logger.info("Closing interface ...")
 con.close()
+logger.info("Interface closed.")
+
+logger.info("Slave stopped.")
