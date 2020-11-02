92793541cba5d410d15d49b75216fff5c395b98a
Leonard Kugis <leonard@kug.is>
2020-11-02 13:52:41 +0100

    Scheduler
    Added ids for tasks.
    Added status readout.
    Fixed recurrent tasks time base on current time instead of due time.

--- PyTrinamicMicro/platforms/motionpy/Scheduler.py
+++ PyTrinamicMicro/platforms/motionpy/Scheduler.py
@@ -6,13 +6,28 @@ Created on 29.10.2020
 @author: LK
 '''
 
+import logging
 from pyb import RTC
+
+class SchedulerStatus(object):
+    pass
+
+class SchedulerStatusStopped(SchedulerStatus):
+    def __str__(self):
+        return "Stopped"
+
+class SchedulerStatusRunning(SchedulerStatus):
+    def __str__(self):
+        return "Running"
+
 
 class Scheduler(object):
 
     def __init__(self, interval=1000):
-        self.tasks = []
-        RTC().wakeup(interval, self.__tick)
+        self.__interval = interval
+        self.__tasks = []
+        self.__status = SchedulerStatusStopped()
+        self.__logger = logging.getLogger(self.__module__)
 
     @staticmethod
     def __is_zero(datetime):
@@ -24,7 +39,7 @@ class Scheduler(object):
         return True
 
     @staticmethod
-    def add_dates(first, second):
+    def __add_dates(first, second):
         first = list(first)
         for i in range(0, len(second)):
             first[i] = first[i] + second[i]
@@ -41,23 +56,37 @@ class Scheduler(object):
 
     def __tick(self, arg):
         current = RTC().datetime()
-        for task in self.tasks:
-            print("current: {}, task: {}".format(current, task["datetime"]))
+        for task in self.__tasks:
             if(self.__due(task["datetime"], current)):
+                self.__logger.debug("Task {} is due. Executing ...".format(task["id"]))
                 task["function"](*task["args"], **task["kwargs"])
                 if(self.__is_zero(task["interval"])):
-                    self.tasks.remove(task)
+                    self.__tasks.remove(task)
                 else:
-                    task["datetime"] = self.__add_dates(task["datetime"], task["interval"])
+                    task["datetime"] = self.__add_dates(current, task["interval"])
+
+    def start(self):
+        RTC().wakeup(self.__interval, self.__tick)
+        self.__status = SchedulerStatusRunning()
+        self.__logger.debug("Scheduler {} started.".format(self))
 
     def stop(self):
         RTC().wakeup(None)
+        self.__status = SchedulerStatusStopped()
+        self.__logger.debug("Scheduler {} stopped.".format(self))
 
-    def register_task(self, datetime, interval, func, *args, **kwargs):
-        self.tasks.append({
+    def status(self):
+        return self.__status
+
+    def register_task(self, id, datetime, interval, func, *args, **kwargs):
+        self.__tasks.append({
+            "id": id,
             "datetime": datetime,
             "interval": interval,
             "function": func,
             "args": args,
             "kwargs": kwargs
         })
+
+    def remove_task(self, id):
+        self.__tasks = [e for e in self.__tasks if e["id"] != id]
