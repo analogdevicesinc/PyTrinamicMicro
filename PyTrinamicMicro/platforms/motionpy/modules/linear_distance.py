6fbd29a56e49f98f7ebcd0ea10903cbcd2c74203
Leonard Kugis <leonard@kug.is>
2020-12-05 22:07:57 +0100

    linear_distance
    Implemented relative positioning

--- PyTrinamicMicro/platforms/motionpy/modules/linear_distance.py
+++ PyTrinamicMicro/platforms/motionpy/modules/linear_distance.py
@@ -13,22 +13,34 @@ class linear_distance(object):
         self.__length = 1000
         self.bounds = []
 
-    def homing(self):
-        self.__mc.rotate(0, 10000)
+    def homing(self, velocity=10000, margin=0.1, margin_hyst=0.5):
+        self.bounds = []
+        self.__mc.rotate(0, velocity)
         distance = self.__sensor.distance(self.__sensor_index)
-        while((0.1 * self.__length) < distance < (0.9 * self.__length)):
+        while((margin * self.__length) < distance < ((1.0 - margin) * self.__length)):
             distance = self.__sensor.distance(self.__sensor_index)
             #print(distance)
         self.__mc.stop(0)
         position = self.__mc.position(0)
         self.bounds.append((distance, position))
-        self.__mc.rotate(0, -10000)
-        if((0.05 * self.__length) < distance < (0.15 * self.__length)): # lower
-            while(distance < (0.9 * self.__length)):
+        self.__mc.rotate(0, -velocity)
+        if(((margin - (margin * margin_hyst)) * self.__length) < distance < ((margin + (margin * margin_hyst)) * self.__length)): # lower
+            while(distance < ((1.0 - margin) * self.__length)):
                 distance = self.__sensor.distance(self.__sensor_index)
         else: # higher
-            while((0.1 * self.__length) < distance):
+            while((margin * self.__length) < distance):
                 distance = self.__sensor.distance(self.__sensor_index)
         self.__mc.stop(0)
         position = self.__mc.position(0)
         self.bounds.append((distance, position))
+        self.bounds.sort(key=lambda x: x[0])
+
+    def position(self, position, acceleration=1000, velocity=100000):
+        if(not(self.bounds)):
+            raise ValueError("Run homing sequence first.")
+
+        position = int(self.bounds[0][1] + ((self.bounds[1][1] - self.bounds[0][1]) * position))
+
+        self.__mc.velocity(0, velocity)
+        self.__mc.acceleration(0, acceleration)
+        self.__mc.moveTo(0, position, 0)
