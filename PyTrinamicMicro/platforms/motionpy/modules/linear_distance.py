1cf1ae3f0a0d8f3d0d285a15bcb7caca3af5de9f
Leonard Kugis <leonard@kug.is>
2020-12-21 02:10:04 +0100

    linear_distance
    adjusted linear_distance for modules and according to new PyTrinamic changes.

--- PyTrinamicMicro/platforms/motionpy/modules/linear_distance.py
+++ PyTrinamicMicro/platforms/motionpy/modules/linear_distance.py
@@ -1,5 +1,3 @@
-from PyTrinamic.ic.TMC5130.TMC5130 import TMC5130
-from PyTrinamicMicro.platforms.motionpy.connections.uart_ic_interface import uart_ic_interface
 from PyTrinamicMicro.platforms.motionpy.modules.hc_sr04_multi import hc_sr04_multi
 from PyTrinamicMicro.platforms.motionpy.modules.MCP23S08 import MCP23S08
 from PyTrinamic.modules.TMCM0960.TMCM0960 import TMCM0960
@@ -7,32 +5,32 @@ import logging
 
 class linear_distance(object):
 
-    def __init__(self, sensor, sensor_index, mc, length=1000):
+    def __init__(self, sensor, sensor_index, module, length=1000):
         self.__sensor = sensor
         self.__sensor_index = sensor_index
-        self.mc = mc
+        self.module = module
         self.__length = 1000
         self.bounds = []
 
     def homing_direct(self, velocity=10000, margin=0.1, margin_hyst=0.5):
         self.bounds = []
-        self.__mc.rotate(0, velocity)
+        self.module.rotate(0, velocity)
         distance = self.__sensor.distance(self.__sensor_index)
         while((margin * self.__length) < distance < ((1.0 - margin) * self.__length)):
             distance = self.__sensor.distance(self.__sensor_index)
             #print(distance)
-        self.__mc.stop(0)
-        position = self.__mc.position(0)
+        self.module.stop(0)
+        position = self.module.getActualPosition(0)
         self.bounds.append((distance, position))
-        self.__mc.rotate(0, -velocity)
+        self.module.rotate(0, -velocity)
         if(((margin - (margin * margin_hyst)) * self.__length) < distance < ((margin + (margin * margin_hyst)) * self.__length)): # lower
             while(distance < ((1.0 - margin) * self.__length)):
                 distance = self.__sensor.distance(self.__sensor_index)
         else: # higher
             while((margin * self.__length) < distance):
                 distance = self.__sensor.distance(self.__sensor_index)
-        self.__mc.stop(0)
-        position = self.__mc.position(0)
+        self.module.stop(0)
+        position = self.module.getActualPosition(0)
         self.bounds.append((distance, position))
         self.bounds.sort(key=lambda x: x[0])
 
@@ -40,17 +38,14 @@ class linear_distance(object):
         if(homing_status == TMCM0960.ENUMs.HOMING_STATUS_INIT):
             self.bounds = []
             #print(velocity)
-            self.mc.writeRegister(self.mc.registers.AMAX, acceleration)
-            self.mc.writeRegister(self.mc.registers.A1, acceleration)
-            self.mc.writeRegister(self.mc.registers.DMAX, acceleration)
-            self.mc.writeRegister(self.mc.registers.D1, acceleration)
-            self.mc.rotate(0, velocity)
+            self.module.setMaxAcceleration(0, acceleration)
+            self.module.rotate(0, velocity)
             homing_status = TMCM0960.ENUMs.HOMING_STATUS_FIRST
         elif(homing_status == TMCM0960.ENUMs.HOMING_STATUS_FIRST):
             distance = self.__sensor.distance(self.__sensor_index)
             if(((margin * length) > distance) or (distance > ((1.0 - margin) * length))):
-                self.bounds.append((distance, self.mc.readRegister(self.mc.registers.XACTUAL, signed=True)))
-                self.mc.rotate(0, -velocity)
+                self.bounds.append((distance, self.module.getActualPosition(0)))
+                self.module.rotate(0, -velocity)
                 homing_status = TMCM0960.ENUMs.HOMING_STATUS_SECOND
         elif(homing_status == TMCM0960.ENUMs.HOMING_STATUS_SECOND):
             distance = self.__sensor.distance(self.__sensor_index)
@@ -61,8 +56,8 @@ class linear_distance(object):
             else: # higher
                 done = ((margin * length) > distance)
             if(done):
-                self.bounds.append((distance, self.mc.readRegister(self.mc.registers.XACTUAL, signed=True)))
-                self.mc.stop(0)
+                self.bounds.append((distance, self.module.getActualPosition(0)))
+                self.module.stop(0)
                 self.bounds.sort(key=lambda x: x[0])
                 homing_status = TMCM0960.ENUMs.HOMING_STATUS_DONE
         return homing_status
@@ -71,15 +66,9 @@ class linear_distance(object):
         if(position is None):
             return self.mc.readRegister(self.mc.registers.XACTUAL, signed=True)
         else:
-            self.mc.writeRegister(self.mc.registers.VMAX, velocity)
-            self.mc.writeRegister(self.mc.registers.VSTART, velocity)
-            self.mc.writeRegister(self.mc.registers.V1, velocity)
-            self.mc.writeRegister(self.mc.registers.VSTOP, velocity)
-            self.mc.writeRegister(self.mc.registers.AMAX, acceleration)
-            self.mc.writeRegister(self.mc.registers.A1, acceleration)
-            self.mc.writeRegister(self.mc.registers.DMAX, acceleration)
-            self.mc.writeRegister(self.mc.registers.D1, acceleration)
-            self.mc.moveTo(0, position)
+            self.module.setMaxAcceleration(0, acceleration)
+            self.module.setMaxVelocity(0, velocity)
+            self.module.moveTo(0, position)
 
     def position_absolute(self, position=None, velocity=10000, acceleration=1000):
         if(position is None):
